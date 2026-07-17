619eca7a72b90af06f0aaa4228ac468ea3f04b48
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-17 08:58:11 +0200

    chore: bump version to 1.1.2

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "rippleeffect",
     fullname    = _("Ripple Effect"),
     description = _("Fill rooms so each number appears once per row/col"),
-    version     = "1.1.1",
+    version     = "1.1.2",
 }

853276235b297103045dc758d82ba49f50f55ef4
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,4 @@
+return {
+    ["Fill rooms so each number appears once per row/col"] = { fr = "Remplissez les zones pour que chaque chiffre apparaisse une fois par ligne/colonne" },
+    ["Ripple Effect"] = { fr = "Ripple Effect" },
+}

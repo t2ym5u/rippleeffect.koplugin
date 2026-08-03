df9e667165b3ed024d0899e62de89cd08c8c9b49
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-03 15:52:00 +0200

    chore: bump version to 1.1.13

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Ripple Effect"),
     description = _("Fill rooms so each number appears once per row/col"),
-    version     = "1.1.12",
+    version     = "1.1.13",
 }

4df3b99dd74e40707dea9ca1ddd177558a472ec5
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:58:56 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,4 +1,4 @@
 return {
-    ["Fill rooms so each number appears once per row/col"] = { fr = "Remplissez les zones pour que chaque chiffre apparaisse une fois par ligne/colonne" },
-    ["Ripple Effect"] = { fr = "Ripple Effect" },
+    ["Fill rooms so each number appears once per row/col"] = { fr = "Remplissez les zones pour que chaque chiffre apparaisse une fois par ligne/colonne", es = "Rellena las zonas para que cada número aparezca una vez por fila/columna", de = "Fülle die Bereiche, sodass jede Zahl einmal pro Zeile/Spalte erscheint" },
+    ["Ripple Effect"] = { fr = "Ripple Effect", es = "Ripple Effect", de = "Ripple Effect" },
 }
